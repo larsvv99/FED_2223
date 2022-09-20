4293085c3f6f63a3db94ecf9454e2938f347a4df
larsvv99 <112852815+larsvv99@users.noreply.github.com>
2022-09-20 23:00:26 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,20 @@
 // JavaScript Document
 console.log("hi");
+
+var menuButton = document.querySelector("header nav button");
+
+menuButton.addEventListener("click", menuOpenen);
+
+function menuOpenen(){
+    var deHeader = document.querySelector("header");
+    deHeader.classList.toggle("menuOpen");
+
+    var deUl = document.querySelector("header nav ul");
+    deUl.classList.toggle("centerMenu");
+
+    var tekstKleurMenu = document.querySelector("header nav ul li a");
+    tekstKleurMenu.classList.toggle("tekstKleurWit");
+
+    var buttonChange = document.querySelector("header button img");
+    buttonChange.classList.toggle("buttonVeranderd")
+  }
